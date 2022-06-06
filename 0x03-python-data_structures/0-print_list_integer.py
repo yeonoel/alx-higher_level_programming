446ced52ac6ca6c16a3905a3298f008030c0b431
yeonoel <yeopevroguinoel@gmail.com>
2022-06-06 04:10:55 +0000

    This function that prints all integers of a list.

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10, 10)
-if number > 0:
-    print("{} is positive".format(number))
-elif number == 0:
-    print("{} is zero".format(number))
-elif number < 0:
-    print("{} is negative".format(number))
+def print_list_integer(my_list=[]):
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
