87a50267c27491ed7c60d6e1a295499f64e44d47
yeonoel <yeopevroguinoel@gmail.com>
2022-06-09 22:45:33 +0000

    This function replaces all occurrences of an element by another in a new list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 
-def uniq_add(my_list=[]):
-    sum = 0
-    for i in set(my_list):
-        sum += i
-    return (sum)
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return (new_list)
