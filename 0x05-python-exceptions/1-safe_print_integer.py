a7d1f3b9493f965ef59bfb3e8b8eeb1cbadfd45f
yeonoel <yeopevroguinoel@gmail.com>
2022-06-20 16:01:33 +0000

    prints an integer with {:d}.format()

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
+    '''
+    prints an integer
+    '''
     try:
-        value = int(value)
         print("{:d}".format(value))
         return (True)
-    except ValueError:
+    except:
         return (False)
