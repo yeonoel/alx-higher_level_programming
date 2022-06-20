06f7e83b2e44e6e853f992b16fbb7c99857b24bb
yeonoel <yeopevroguinoel@gmail.com>
2022-06-20 17:17:44 +0000

    prints the first x elements of a list and only integers.

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    j = 0
+    try:
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end='')
+                j += 1
+    except(TypeError, ValueError):
+        continue
+    print()
+    return (j)
