94c42a7137efe61878143045e08d5a9a0a1ee805
yeonoel <yeopevroguinoel@gmail.com>
2022-07-04 22:57:23 +0000

    Full rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -23,6 +23,6 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """Return the print() and str() representation of the Rectangle. """
-        string = "[" + str(self.__class__.__name__) + "]"
+        string = "[" + str(self.__class__.__name__) + "] "
         string += str(self.__width) + "/" + str(self.__height)
         return (string)
