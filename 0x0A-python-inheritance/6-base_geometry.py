af416ac942329af53be46d7d037ac3efee01090d
yeonoel <yeopevroguinoel@gmail.com>
2022-07-04 20:41:24 +0000

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" BaseGeometry"""
+
+
+class BaseGeometry:
+    """ the Geometry Base"""
+    def __init__(self):
+        """ Initialize BaseGeometry """
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
