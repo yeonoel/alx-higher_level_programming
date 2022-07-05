88e8342f6cc46da28bc08a458a506a3ad50f5a03
yeonoel <yeopevroguinoel@gmail.com>
2022-07-05 17:53:25 +0000

    This function read a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Read file."""
+
+
+def read_file(filename=""):
+    """Read text file UTF8 and prints.
+    Arg:
+        filename(file): the file that is read ant prints
+    """
+    with open(filename, encoding="utf-8") as f:
+        text = f.read()
+        print(text)
