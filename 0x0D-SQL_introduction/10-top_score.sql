1907cf2ed5d771d7bd79b27a0ded9ac20504b44d
yeonoel <yeopevroguinoel@gmail.com>
2022-08-11 20:57:10 +0000

    List all records of the table

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- Lists all records of the table second_table
+-- Results should display both the score and the name
+-- Score should be ordered by score (top first)
+SELECT score, name
+FROM `second_table`
+ORDER BY `score` DESC;
