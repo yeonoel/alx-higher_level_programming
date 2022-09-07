1fb0e6c55e84736d306e9a0e7573917785f8a41d
yeonoel <yeopevroguinoel@gmail.com>
2022-09-07 14:34:34 +0000

    This cript return the second buggest number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ if (process.argv.lenght < 2) {
   console.log(0);
 } else {
   const args = process.argv.map(Number)
-    .slice(2, process.argv.lenght)
+    .slice(2, process.argv.length)
     .sort((a, b) => a - b);
   console.log(args[args.length - 2]);
 }
