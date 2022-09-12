34e27aecb0b035ba91527443417f58b33cfab98f
yeonoel <yeopevroguinoel@gmail.com>
2022-09-12 16:16:42 +0000

    Cities by states

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This script lists all cities from the database hbtn_0e_4_usa"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name\
+                FROM cities INNER JOIN states\
+                ON cities.id = states.id ORDER BY id ASC")
+    cities = cur.fetchall()
+    for citie in cities:
+        print(citie)
+    cur.close()
+    db.close()
