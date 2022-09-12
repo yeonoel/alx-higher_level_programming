cfc137ededc3e6171938c37250b36426bdf8ce7d
yeonoel <yeopevroguinoel@gmail.com>
2022-09-12 03:09:30 +0000

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,5 +9,10 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states ORDER BY id""")
-    [print(state) for state in cur.fetchall() if state[1] == sys.argv[4]]
+    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY id".format(sys.argv[4]))
+    states = cur.fetchall()
+    for state in states: 
+        if state[1] == sys.argv[4]:
+            print(state)
+    cur.close()
+    db.close()
