be193a87041274eaef6172201ec2b3f7642ba1c5
yeonoel <yeopevroguinoel@gmail.com>
2022-09-13 12:42:18 +0000

    Task 13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(id):
+    for state in session.query(State).order_by(State.id):
         if "a" in state.name:
             session.delete(state)
     session.commit()
