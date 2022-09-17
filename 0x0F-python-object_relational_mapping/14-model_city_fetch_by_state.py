2f8a7db8931b8e8e78d9ac501e7351fb2bac01de
yeonoel <yeopevroguinoel@gmail.com>
2022-09-17 12:45:13 +0000

    Cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for city, state in session.query(City, State) \
+    for state, city in session.query(City, State) \
                               .filter(City.state_id == State.id) \
                               .order_by(City.id).all():
         print("{}: ({}) {}".format(state.name, city.id, city.name))
