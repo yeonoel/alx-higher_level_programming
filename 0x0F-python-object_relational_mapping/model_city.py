69106d25640c46cda169cd75cffe3030d3812d7d
yeonoel <yeopevroguinoel@gmail.com>
2022-09-17 12:51:09 +0000

    Cities in state

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -18,6 +18,7 @@ class City(Base):
         state_id (sqlalchemy.String): The city's state id.
     """
     __tablename__ = "cities"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, autoincrement=True,
+                primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
