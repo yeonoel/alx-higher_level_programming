863b54ad1bcae04f3dfb1e44d2090975c7ad3d2a
yeonoel <yeopevroguinoel@gmail.com>
2022-09-23 13:48:18 +0000

    This script takes in a URL, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #/bin/bash
-# This script takes in a URL, sends a GET request to the URL, and displays the body of the response
+# This script takes in a URL, sends a GET request to the URL
 curl -s "$1" -X GET -L
