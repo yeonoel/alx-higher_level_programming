7125e65340d2ea330b6bf3aec53e8350cc22906d
yeonoel <yeopevroguinoel@gmail.com>
2022-09-23 13:07:18 +0000

    cURL body size

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# takes in a URL, sends a request to that URL, and
+# displays the size of the body of the response.
+# The size must be displayed in bytes
+
+curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
