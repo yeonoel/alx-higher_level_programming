f3962ede91117bc24f791bce375bf18ee059d1f6
yeonoel <yeopevroguinoel@gmail.com>
2022-09-24 01:26:00 +0000

    sends a request to the URL and displays the body of the response

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
 
     try:
         with urllib.request.urlopen(sys.argv[1]) as response:
-            print(response.read().decode("utf-8"wq))
+            print(response.read().decode("utf-8"))
     except HTTPError as e:
         print("Error code: {}".format(e.code))
