efbf73abc9758ababa8ab51071a237b3de09d518
yeonoel <yeopevroguinoel@gmail.com>
2022-09-24 00:05:14 +0000

    This script fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,6 +3,7 @@
 
 if __name__ == "__main__":
     import urllib.request
+
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
             as response:
         html = response.read()
