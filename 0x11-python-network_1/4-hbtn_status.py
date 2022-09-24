42499a0d901982a7ad2e20c90b40e3b2d040282a
yeonoel <yeopevroguinoel@gmail.com>
2022-09-24 02:02:05 +0000

    Task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@
 import requests
 
 
-if "__name__" == __main__:
+if __name__ == "__main__":
     resp = request.get('https://alx-intranet.hbtn.io/status')
     print('Body response:')
     print("\t- type: {}".format(type(resp.text)))
