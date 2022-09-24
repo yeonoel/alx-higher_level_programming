1c5ae28fb7872bbe6424537693ec90b1f88b865f
yeonoel <yeopevroguinoel@gmail.com>
2022-09-24 01:08:58 +0000

    Post email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""ends a POST request to the passed URL with the email as a parameter"""
+import urllib.request
+import urllib.parse
+import sys
+
+
+url = sys.argv[1]
+value = {'email': sys.argv[2]}
+data = urllib.parse.urlencode(value)
+data = data.encode('ascii')
+req = urllib.request.Request(url, data)
+with urllib.request.urlopen(req) as response:
+    print(response.read().decode("utf-8"))
