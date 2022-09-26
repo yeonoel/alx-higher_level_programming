c62583592aa745c556511fba394edcf401457e7f
yeonoel <yeopevroguinoel@gmail.com>
2022-09-26 01:19:50 +0000

    Update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,20 +4,19 @@
 
 def find_peak(list_of_integers):
     """Finds a peak in list_of_integers"""
-    if list_of_integers == []:
+
+    if (list_of_integers == []):
         return None
 
-    size = len(list_of_integers)
-    if size == 1:
-        return list_of_integers[0]
-    elif size == 2:
-        return max(list_of_integers)
+    left = 0
+    right = len(list_of_integers) - 1
 
-    mid = int(size / 2)
-    peak = list_of_integers[mid]
-    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
-        return peak
-    elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid + 1:])
+    while (left < right):
+        mid = left + (right - left) // 2
+
+        if (list_of_integers[mid] < list_of_integers[mid + 1]):
+            left = mid + 1
+        else:
+            right = mid
+
+    return (list_of_integers[left])
