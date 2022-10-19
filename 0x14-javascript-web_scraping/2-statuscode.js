c6df9ac67e0cfe4b87a8aae40baf2f174458c30f
yeonoel <yeopevroguinoel@gmail.com>
2022-10-19 17:01:02 +0000

    This script reads and prints the content of a file.

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,6 +5,6 @@ const request = require('request');
 request(process.argv[2], function (error, response) {
   if (error) {
     console.error(error);
-  } 
+  }
   console.log('code:', response && response.statusCode);
 });
