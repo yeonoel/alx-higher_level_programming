8c848ff85d011da40d703af138844f556783ff20
yeonoel <yeopevroguinoel@gmail.com>
2022-10-20 16:42:04 +0000

    This scriipt prints all characters of a Star Wars movie:

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,9 +2,9 @@
 
 const request = require('request');
 
-request('http://swapi.co/api/films/' + process.argv[2], function (err, response, body) {
-  if (err) {
-    console.error(err);
+request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
   }
   JSON.parse(body).characters.forEach(function (url, callbackfunc) {
     request(url, (error, response, body) => {
